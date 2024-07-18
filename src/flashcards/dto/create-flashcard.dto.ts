4b7b989bd925e5ac29694c7797a768e7222e426a
Lucas Schimidt <llourencoschimidt@gmail.com>
2024-07-18 20:58:08 -0300

    CRUD API operations

--- src/flashcards/dto/create-flashcard.dto.ts
+++ src/flashcards/dto/create-flashcard.dto.ts
@@ -1,9 +1,28 @@
+import { IsString, IsArray, ArrayNotEmpty, IsNotEmpty, IsNumber, IsDate, isString } from 'class-validator';
+import { IsISODateString } from 'src/common/decorators/is-iso-date-string';
+
 export class CreateFlashcardDto {
+
+  @IsString()
   title: string;
+
+  @IsString()
   description: string;
+
+  @IsString()
   subject: string;
+
+  @IsNumber()
   flashcard_reviews: number;
+
+  @IsString()
+  @IsISODateString()
   next_review: Date;
+
+  @IsString()
+  @IsISODateString()
   created_at: Date;
+
+  @IsString()
   content: string;
 }
--- src/flashcards/flashcards.service.ts
+++ src/flashcards/flashcards.service.ts
@@ -1,9 +1,9 @@
 import { HttpException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateFlashcardDto } from './dto/create-flashcard.dto';
 import { UpdateFlashcardDto } from './dto/update-flashcard.dto';
-import { InjectRepository } from '@nestjs/typeorm';
 import { Flashcard } from './entities/flashcard.entity';
-import { Repository } from 'typeorm';
 
 @Injectable()
 export class FlashcardsService {
@@ -20,38 +20,111 @@ export class FlashcardsService {
       await this.flashcardRepository.save(newFlashcard);
 
     } catch(err) {
-      return err
+      throw new HttpException(`Erro ao salvar flashcard no banco de dados.`, 400)
     }
 
     return 'Flashcard criado com sucesso!'
   }
 
-  findAll() {
-    return `This action returns all flashcards`;
+  //Metodo que puxa TODOS os flashcards do banco de dados:
+  async findAll(): Promise<Flashcard[]> {
+    try {
+      const flashcards = await this.flashcardRepository.find();
+      return flashcards
+
+    } catch(err) {
+      throw new HttpException(`Erro ao retornar os flashcards do banco de dados: ${err}`, 400)
+    }
   }
 
-  findById(id: number) {
-    return `This action returns a #${id} flashcard`;
+  //Metodo que puxa flashcard pelo id:
+  async findById(id): Promise<Flashcard> {
+
+    const flashcard = await this.flashcardRepository.findOne({
+      where: { id: id },
+    });
+
+    if (!flashcard) {
+      throw new HttpException(`Flashcard de id: ${id}, não encontrado.`, 404)
+    }
+
+    return flashcard
   }
 
-  findByName(id: number) {
-    return `This action returns a #${id} flashcard`;
+  //Metodo que puxa flashcard pelo titulo:
+  async findByName(name:string): Promise<Flashcard[]> {
+
+    const flashcards = await this.flashcardRepository.find({
+      where: { title: name },
+    });
+
+    if (!flashcards) {
+      throw new HttpException(`Nenhum flashcard de titulo: ${name}, foi encontrado.`, 404)
+    }
+
+    return flashcards
   }
 
-  findBySubject(id: number) {
-    return `This action returns a #${id} flashcard`;
+  //Metodo que puxa flashcard pelo assunto:
+  async findBySubject(subject:string): Promise<Flashcard[]> {
+
+    const flashcards = await this.flashcardRepository.find({
+      where: { subject: subject },
+    });
+
+    if (flashcards.length == 0) {
+      throw new HttpException(`Nenhum flashcard de titulo: ${subject}, foi encontrado.`, 404)
+    }
+
+    return flashcards
   }
 
+  //Metodo que puxa flashcard pela data de proxima visualização:
+  async findByDate(date: string): Promise<Flashcard[]> {
 
-  findByDate(id: number) {
-    return `This action returns a #${id} flashcard`;
+    const formated_date: Date = new Date(date);
+
+    const flashcards = await this.flashcardRepository.find({
+      where: { next_review: formated_date },
+    });
+
+    if (flashcards.length == 0) {
+      throw new HttpException(`Nenhum flashcard programado para: ${date}, foi encontrado.`, 404)
+    }
+
+    return flashcards
   }
 
-  update(id: number, updateFlashcardDto: UpdateFlashcardDto) {
-    return `This action updates a #${id} flashcard`;
+  //Metodo que atualiza um flashcard pelo id:
+  async update(id: number, updateFlashcardDto: UpdateFlashcardDto) {
+
+    const result = await this.flashcardRepository.update(id, updateFlashcardDto);
+
+    if (!result) {
+      throw new HttpException(`Flashcard de id: ${id}, não encontrado.`, 404);
+    } else if(result.affected === 0) {
+      throw new HttpException(`Erro na hora de fazer update do flashcard`, 404);
+    }
+
+    return 'Flashcard atualizado com sucesso!'
+    
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} flashcard`;
+  //Metodo que deleta um flashcard pelo id:
+  async remove(id: number) {
+
+    const flashcard = await this.flashcardRepository.findOne({
+      where: { id: id },
+    });
+
+    if (!flashcard) {
+      throw new HttpException(`Flashcard de id: ${id}, não encontrado.`, 404)
+    }
+
+    this.flashcardRepository.delete(id);
+
+    return 'Flashcard deletado com sucesso!'
+    
+    
   }
 }
